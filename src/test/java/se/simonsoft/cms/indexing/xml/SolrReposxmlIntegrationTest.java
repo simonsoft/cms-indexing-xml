18a0bb8fd5d6f58782cf300ea6e5d89e6fccb6de
Staffan Olsson <staffan@repos.se>
2012-09-03 09:16:20 +0000

    Solved "Can't find resource 'reposxml/conf/solrconfig.xml' in classpath or 'reposxml/collection1/conf/'" by placing the solr core in test classpath. I think this also has the benefit of the core being available for test from other modules, cms-xmltracking in this case. Have a test setup working now both for direct queries to solr and for SolrServer injection into our services. Note that test methods will interfere with each other, but the index is reset for each test case run.

--- src/test/java/se/simonsoft/cms/indexing/xml/SolrReposxmlIntegrationTest.java
+++ src/test/java/se/simonsoft/cms/indexing/xml/SolrReposxmlIntegrationTest.java
@@ -2,13 +2,27 @@ package se.simonsoft.cms.indexing.xml;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.apache.solr.SolrTestCaseJ4;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrInputDocument;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
+ * Verify features of the actual index, without using our abstractions.
+ * This test should match stuff that we rely on for direct queries to solr from various places.
+ * Create new test methods per integration case.
  * 
+ * Using solr test-framework instead of http://wiki.apache.org/solr/Solrj#EmbeddedSolrServer-1
+ * to get their temp file management etc.
  * 
+ * http://lucene.apache.org/solr/api-4_0_0-BETA/test-framework/index.html?org/apache/solr/SolrTestCaseJ4.html
  * http://blog.florian-hopf.de/2012/06/running-and-testing-solr-with-gradle.html
  * 
  * Lucene test framework requires "assertions"
@@ -20,18 +34,42 @@ import org.junit.Test;
  */
 public class SolrReposxmlIntegrationTest extends SolrTestCaseJ4 {
 
-	static {
-		System.setProperty("solr.solr.home", "./src/main/solr");
-	}
+	public static String solrhome = "se/simonsoft/cms/indexing/xml/solr";
 
 	@BeforeClass
 	public static void beforeTests() throws Exception {
-		SolrTestCaseJ4.initCore("reposxml/conf/solrconfig.xml", "reposxml/conf/schema.xml", "reposxml");
+		try {
+			SolrTestCaseJ4.initCore(solrhome + "/reposxml/conf/solrconfig.xml", solrhome + "/reposxml/conf/schema.xml",
+					"src/test/resources/" + solrhome); // has to be in classpath because "collection1" is hardcoded in TestHarness initCore/createCore
+		} catch (Exception e) {
+			System.out.println("getSolrConfigFile()=" + getSolrConfigFile());
+			System.out.println("testSolrHome=" + testSolrHome);
+			throw e;
+		}
 	}
-
+	
+	/**
+	 * @return instance for injection when integration testing our logic with solr, for index testing we do fine with SolrTestCaseJ4 helper methods
+	 */
+	public SolrServer getTestServer() {
+		return new EmbeddedSolrServer(h.getCoreContainer(), h.getCore().getName()); // cache between tests?
+	}
+	
 	@Test
-	public void testIndexEmptyFromStart() {
-		assertQ("test query on empty index", req("*:*"), "//result[@numFound='0']");
+	public void testCommon() throws SolrServerException, IOException {
+		// verify test setup
+		assertQ("index should be empty on each test run", req("*:*"), "//result[@numFound='0']");
+		// reference use of solr api
+		SolrServer solr = getTestServer();
+		SolrInputDocument doc1 = new SolrInputDocument();
+		doc1.addField("id", "x");
+		doc1.addField("name", "x");
+		solr.add(doc1);
+		solr.commit();
+		QueryResponse query = solr.query(new SolrQuery("*:*"));
+		assertEquals(1, query.getResults().getNumFound());
+		// verify test setup
+		assertQ("index should be empty on each test run", req("*:*"), "//result[@numFound='1']");
 	}
 
 }
