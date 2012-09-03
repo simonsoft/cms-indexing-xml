8799137872315f4969d72c30b12754c54c96a6a4
Staffan Olsson <staffan@repos.se>
2012-09-03 07:52:33 +0000

    Solved "must enable assertions" and learned something new about java

--- src/test/java/se/simonsoft/cms/indexing/xml/SolrReposxmlIntegrationTest.java
+++ src/test/java/se/simonsoft/cms/indexing/xml/SolrReposxmlIntegrationTest.java
@@ -11,25 +11,27 @@ import org.junit.Test;
  * 
  * http://blog.florian-hopf.de/2012/06/running-and-testing-solr-with-gradle.html
  * 
- * 
- *
+ * Lucene test framework requires "assertions"
+ *  - http://docs.oracle.com/javase/1.4.2/docs/guide/lang/assert.html#enable-disable
+ *  - http://java.sun.com/developer/technicalArticles/JavaLP/assertions/
+ *  - http://stackoverflow.com/questions/5509082/eclipse-enable-assertions
+ *  - http://maven.apache.org/plugins/maven-surefire-plugin/test-mojo.html#enableAssertions
+ *  - Eclipse > Preferences > Java > Junit > Append -ea to JVM arguments ...
  */
-public class SolrReposxmlIntegrationTest {
+public class SolrReposxmlIntegrationTest extends SolrTestCaseJ4 {
 
 	static {
 		System.setProperty("solr.solr.home", "./src/main/solr");
 	}
-	
+
 	@BeforeClass
 	public static void beforeTests() throws Exception {
-		System.out.println("hello");
-		SolrTestCaseJ4.initCore("reposxml/conf/solrconfig.xml", "reposxml/conf/schema.xml", "reposxml/");
+		SolrTestCaseJ4.initCore("reposxml/conf/solrconfig.xml", "reposxml/conf/schema.xml", "reposxml");
 	}
-	
+
 	@Test
-	public void testSomething() {
-		System.out.println("here");
-		fail("hepp");
+	public void testIndexEmptyFromStart() {
+		assertQ("test query on empty index", req("*:*"), "//result[@numFound='0']");
 	}
 
 }
