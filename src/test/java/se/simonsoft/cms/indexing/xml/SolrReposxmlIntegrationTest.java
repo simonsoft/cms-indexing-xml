658a27db6d3857af8b1b4d71f459f4eaa1c9112b
Staffan Olsson <staffan@repos.se>
2012-09-03 07:30:29 +0000

    Trying to set up a Solr test case. We'll need reliable solr based unit tests for TMS features once we start depending on solr's analysis, fuzzy etc.

---
+++ src/test/java/se/simonsoft/cms/indexing/xml/SolrReposxmlIntegrationTest.java
@@ -0,0 +1,35 @@
+package se.simonsoft.cms.indexing.xml;
+
+import static org.junit.Assert.*;
+
+import org.apache.solr.SolrTestCaseJ4;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * 
+ * 
+ * http://blog.florian-hopf.de/2012/06/running-and-testing-solr-with-gradle.html
+ * 
+ * 
+ *
+ */
+public class SolrReposxmlIntegrationTest {
+
+	static {
+		System.setProperty("solr.solr.home", "./solrhome");
+	}
+	
+	@BeforeClass
+	public static void beforeTests() throws Exception {
+		System.out.println("hello");
+		SolrTestCaseJ4.initCore("src/main/solr/reposxml/conf/solrconfig.xml", "src/main/solr/reposxml/conf/schema.xml");
+	}
+	
+	@Test
+	public void testSomething() {
+		System.out.println("here");
+		fail("hepp");
+	}
+
+}
