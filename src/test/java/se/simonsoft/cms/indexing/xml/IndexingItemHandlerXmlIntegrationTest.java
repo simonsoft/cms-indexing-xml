f214ba4b003a1198cae1cc7cbd2fe106c70c9036
Staffan Olsson <staffan@repos.se>
2013-09-26 04:51:44 +0000

    Converted the integration test to the filexml backend, oh what a relief. Conversion: {{{ svn list -R --xml . > list.xml svn proplist -R --xml -v . > proplist.xml saxonb-xslt list.xml ~/workspace/cms-backend-filexml/src/test/resources/se/simonsoft/cms/backend/filexml/mergetorepository.xsl > repository.xml rm list.xml rm repository.xml rm .svn -Rf }}} I was dubious about the required configuration steps in a recent commit to cms-xmltracking, but as a setUp method the current indexing config doesn't look that bad. Separation between indexing and backend is clean, and the test pretty much specifies the path do the dataset. For pretranslation we need the injector from indexing so we can build a pretranslate context, but maybe we should only share the SolrServer "reposxml" instance and set up the rest separately for pretranslation even if they share some services.

--- src/test/java/se/simonsoft/cms/indexing/xml/IndexingItemHandlerXmlIntegrationTest.java
+++ src/test/java/se/simonsoft/cms/indexing/xml/IndexingItemHandlerXmlIntegrationTest.java
@@ -17,68 +17,45 @@ package se.simonsoft.cms.indexing.xml;
 
 import static org.junit.Assert.*;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-import javax.xml.transform.Source;
-import javax.xml.transform.stream.StreamSource;
-
-import org.apache.commons.io.FileUtils;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.common.SolrDocumentList;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.tmatesoft.svn.core.SVNDepth;
-import org.tmatesoft.svn.core.SVNException;
-import org.tmatesoft.svn.core.SVNPropertyValue;
-import org.tmatesoft.svn.core.wc.SVNClientManager;
-import org.tmatesoft.svn.core.wc.SVNCopySource;
-import org.tmatesoft.svn.core.wc.SVNRevision;
 
 import se.repos.testing.indexing.SvnTestIndexing;
 import se.repos.testing.indexing.TestIndexOptions;
+import se.simonsoft.cms.backend.filexml.CmsRepositoryFilexml;
+import se.simonsoft.cms.backend.filexml.FilexmlRepositoryReadonly;
+import se.simonsoft.cms.backend.filexml.FilexmlSourceClasspath;
+import se.simonsoft.cms.backend.filexml.testing.ReposTestBackendFilexml;
 import se.simonsoft.cms.indexing.xml.fields.IndexFieldDeletionsToSaveSpace;
 import se.simonsoft.cms.indexing.xml.fields.XmlIndexFieldElement;
 import se.simonsoft.cms.indexing.xml.fields.XmlIndexFieldExtractionChecksum;
 import se.simonsoft.cms.indexing.xml.fields.XmlIndexIdAppendTreeLocation;
 import se.simonsoft.cms.indexing.xml.solr.XmlIndexWriterSolrj;
-import se.simonsoft.cms.testing.svn.CmsTestRepository;
-import se.simonsoft.cms.testing.svn.SvnTestSetup;
 import se.simonsoft.xmltracking.source.XmlSourceReader;
 import se.simonsoft.xmltracking.source.jdom.XmlSourceReaderJdom;
 import se.simonsoft.xmltracking.source.saxon.IndexFieldExtractionCustomXsl;
-import se.simonsoft.xmltracking.source.saxon.XmlMatchingFieldExtractionSource;
 import se.simonsoft.xmltracking.source.saxon.XmlMatchingFieldExtractionSourceDefault;
 
 public class IndexingItemHandlerXmlIntegrationTest {
 
 	/// test framework from cms-backend-svnkit
 	
-	private File wc = null;
-	
-	private SVNClientManager clientManager = null;
-	
-	private CmsTestRepository repo = null;
-	
-	private SvnTestSetup setup = SvnTestSetup.getInstance();
 	private SvnTestIndexing indexing = null;
-	
+
+	/**
+	 * Manual dependency injection. We should think twice before we copy this config to other tests,
+	 * because as soon as we have >1 integration tests it will be a maintenance problem if we for example add handlers.
+	 */
 	@Before
-	public void setUp() throws IOException, SVNException {
-		repo = setup.getRepository();
-		wc = new File(repo.getAdminPath(), "wc"); // inside repository, same cleanup
-
-		clientManager = SVNClientManager.newInstance(null, repo.getSvnkit().getAuthenticationManager());
-		
-		setUpIndexing(repo);
-	}
-
-	private void setUpIndexing(CmsTestRepository repo) {
+	public void setUpIndexing() {
 		TestIndexOptions indexOptions = new TestIndexOptions().itemDefaults();
 		indexOptions.addCore("reposxml", "se/simonsoft/cms/indexing/xml/solr/reposxml/**");
 		indexing = SvnTestIndexing.getInstance(indexOptions);
@@ -102,96 +79,22 @@ public class IndexingItemHandlerXmlIntegrationTest {
 	
 	@After
 	public void tearDown() throws IOException {
-		setup.tearDown();
 		indexing.tearDown();
 	}
 	
-	// test helpers from cms-backend-svnkit, using the file URL, might need porting to use http for hooks to get executed properly
-	
-	private void svncheckout() throws SVNException {
-		clientManager.getUpdateClient().doCheckout(repo.getUrlSvnkit(), wc, SVNRevision.HEAD, SVNRevision.HEAD, SVNDepth.INFINITY, false);
-	}
-	
-	private void svnupdate() throws SVNException {
-		clientManager.getUpdateClient().doUpdate(wc, SVNRevision.HEAD, SVNDepth.INFINITY, false, true);
-	}
-	
-	private long svncommit(String comment) throws SVNException {
-		return clientManager.getCommitClient().doCommit(
-				new File[]{wc}, false, comment, null, null, false, false, SVNDepth.INFINITY).getNewRevision();
-	}
-	
-	private long svncommit() throws SVNException {
-		return svncommit("");
-	}
-	
-	private void svnpropset(File path, String propname, String propval) throws SVNException {
-		clientManager.getWCClient().doSetProperty(path, propname, SVNPropertyValue.create(propval), false, SVNDepth.EMPTY, null, null);
-	}
-
-	private void svnadd(File... paths) throws SVNException {
-		clientManager.getWCClient().doAdd(
-				paths, true, false, false, SVNDepth.INFINITY, true, true, true);
-	}
-	
-	private void svnrm(File path) throws SVNException {
-		if (!path.exists()) {
-			throw new IllegalArgumentException("For tests the deletion target must exist");
-		}
-		clientManager.getWCClient().doDelete(path, false, true, false);
-	}
-	
-	private void svncopy(File from, File to, boolean deleteOriginal) throws SVNException {
-		svncopy(from, SVNRevision.HEAD, to, deleteOriginal);
-	}
-	
-	private void svncopy(File from, SVNRevision fromRevision, File to, boolean deleteOriginal) throws SVNException {
-		SVNCopySource source = new SVNCopySource(fromRevision, fromRevision, from);
-		clientManager.getCopyClient().doCopy(new SVNCopySource[]{source}, to, deleteOriginal, false, true);
-	}
-	
-	private void svncopy(File from, File to) throws SVNException {
-		svncopy(from, to, false);
-	}
-
-	private void svncopy(File from, long fromRevision, File to) throws SVNException {
-		svncopy(from, SVNRevision.create(fromRevision), to, false);
-	}
-	
-	private void svnmove(File from, File to) throws SVNException {
-		//svncopy(from, to, true);
-		clientManager.getMoveClient().doMove(from, to);
-	}	
-	
 	@Test
-	public void test() throws Exception {
-
-		String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-		String dtd = "<!DOCTYPE document PUBLIC \"-//Simonsoft//DTD TechDoc Base V1.0 Techdoc//EN\" \"techdoc.dtd\">\n";
-		String begin = "<doc>";
-		String end = "</doc>";
-		String part1a = "<elem>text</elem><elem id=\"e2\">elem <inline>text</inline></elem>";
+	public void testTinyInline() throws Exception {
+		FilexmlSourceClasspath repoSource = new FilexmlSourceClasspath("se/simonsoft/cms/indexing/xml/datasets/tiny-inline");
+		CmsRepositoryFilexml repo = new CmsRepositoryFilexml("http://localtesthost/svn/testaut1", repoSource);
+		FilexmlRepositoryReadonly filexml = new FilexmlRepositoryReadonly(repo);
 		
-		svncheckout();
-
-		// first produce some revisions so we see that isOverwritten handling works
-		File f1 = new File(wc, "test1.xml");
-		FileUtils.writeStringToFile(f1, xml + begin + part1a + end);
-		svnadd(f1);
-		svncommit();
-		
-		FileUtils.writeStringToFile(f1, xml + dtd + begin + part1a + end);
-		svncommit();
-		
-		// now sync
-		indexing.enable(repo);
+		indexing.enable(new ReposTestBackendFilexml(filexml));
 		
 		SolrServer reposxml = indexing.getCore("reposxml");
 		SolrDocumentList x1 = reposxml.query(new SolrQuery("*:*")).getResults();
 		assertEquals(4, x1.getNumFound());
 	
 		// now produce more revisions
-		
 		
 	}
 
