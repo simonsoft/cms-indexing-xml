7ff7734e6988e21b001837f0314eb13865cb13ab
Jonathan Andersson <jonathan.andersson@simonsoft.se>
2015-11-05 16:58:16 +0000

    #898 Handler for title indexing implemented. The handler holds the possible title-keys.

---
+++ src/main/java/se/simonsoft/cms/indexing/abx/HandlerTitleSelection.java
@@ -0,0 +1,70 @@
+package se.simonsoft.cms.indexing.abx;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import se.repos.indexing.IndexingDoc;
+import se.repos.indexing.IndexingItemHandler;
+import se.repos.indexing.item.HandlerPathinfo;
+import se.repos.indexing.item.HandlerProperties;
+import se.repos.indexing.item.IndexingItemProgress;
+
+public class HandlerTitleSelection implements IndexingItemHandler{
+
+	private static final String TITLE_FIELD = "title";
+	private static final Logger logger = LoggerFactory.getLogger(HandlerTitleSelection.class);
+	private Set<String> fieldTitleKeys = new LinkedHashSet<String>();
+	/***
+	 * Constructor populates a set with possible title properties keys.
+	 */
+	public HandlerTitleSelection() {
+		super();
+		fieldTitleKeys.add("prop_cms.title");
+		fieldTitleKeys.add("embd_title");
+		fieldTitleKeys.add("xmp_dc.title");
+		fieldTitleKeys.add("xmp_dc.subject");
+		fieldTitleKeys.add("xmp_dc.description");
+	}
+
+	/***
+	 * Looks after possible title values with given keys. The first hit will be indexed.
+	 * The order of the set decides the priority.
+	 */
+	@Override
+	public void handle(IndexingItemProgress progress) {
+		IndexingDoc doc = progress.getFields();
+		
+		for(String key: getFieldKeys()) {
+			if(doc.containsKey(key)){
+			 	String value = doc.getFieldValues(key).iterator().next().toString();
+				if(value != null && !value.equals("")) {
+					doc.setField(TITLE_FIELD, value);
+					logger.info("Indexing value from: " + key + ", the value: " + value);
+					break;
+				}
+			}
+		}
+	}
+
+	@SuppressWarnings("serial")
+	@Override
+	public Set<Class<? extends IndexingItemHandler>> getDependencies() {
+		return new HashSet<Class<? extends IndexingItemHandler>>() {{
+			add(HandlerPathinfo.class);
+			add(HandlerProperties.class);
+		}};
+	}
+
+	public void setFieldKeys(Set<String> fieldTitleKeys) {
+		this.fieldTitleKeys = fieldTitleKeys;
+	}
+
+	public Set<String> getFieldKeys() {
+		return fieldTitleKeys;
+	}
+
+}
