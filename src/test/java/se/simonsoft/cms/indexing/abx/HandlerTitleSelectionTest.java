a7f0ad83f2c39d22073588cbd618bf60e66a0a5d
Thomas Åkesson <thomas.akesson@simonsoft.se>
2016-06-22 19:09:30 +0000

    #898 Rename XSL extracted title field to 'embd_xml_title'. Indexing will fail if field name overlaps with Tika extraction.

--- src/test/java/se/simonsoft/cms/indexing/abx/HandlerTitleSelectionTest.java
+++ src/test/java/se/simonsoft/cms/indexing/abx/HandlerTitleSelectionTest.java
@@ -19,7 +19,6 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
 import java.util.Set;
 
 import org.junit.Test;
@@ -34,23 +33,10 @@ public class HandlerTitleSelectionTest {
 	public void thereShoudlBeFiveFieldKeys() {
 		HandlerTitleSelection handler = new HandlerTitleSelection();
 		Set<String> fieldNames = handler.getFieldKeys();
-		assertEquals("Should contain 5 fields",5 ,fieldNames.size());
-		assertEquals("prop_cms.title" ,fieldNames.iterator().next().toString());
+		assertEquals("Should contain 6 fields", 6, fieldNames.size());
+		assertEquals("prop_cms.title", fieldNames.iterator().next().toString());
 	}
 	
-	@Test
-	public void priorityOfFieldKeys() {
-		HandlerTitleSelection handler = new HandlerTitleSelection();
-		Set<String> fieldNames = handler.getFieldKeys();
-		ArrayList<String> sortedKeys = new ArrayList<String>();
-		sortedKeys.addAll(fieldNames);
-		
-		assertEquals("prio 1","prop_cms.title" , sortedKeys.get(0));
-		assertEquals("prio 2","embd_title" , sortedKeys.get(1));
-		assertEquals("prio 3","xmp_dc.title", sortedKeys.get(2));
-		assertEquals("prio 4","xmp_dc.subject" , sortedKeys.get(3));
-		assertEquals("prio 5","xmp_dc.description" , sortedKeys.get(4));
-	}
 	
 	@Test
 	public void shouldIndexKeyValueWithHighestPrio() {
