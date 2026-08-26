5d96be05227fc53752064d211dafd7692f5f9543
Oscar-Lilja-Simonsoft <oscar.lilja@simonsoft.se>
2026-08-26 14:42:41 +0200

    Migrate release translation test to Quarkus
    Load the releasetranslation dataset through SVN Dev Service and verify the existing reposxml assertions using the named Solr client. Remove the corresponding legacy Filexml/repos-testing test method.

--- src/test/java/se/simonsoft/cms/indexing/xml/HandlerXmlIntegrationTest.java
+++ src/test/java/se/simonsoft/cms/indexing/xml/HandlerXmlIntegrationTest.java
@@ -99,40 +99,6 @@ public class HandlerXmlIntegrationTest {
 		assertEquals("Should not have cleared other repositories", 1, reposxml.query(new SolrQuery("*:*")).getResults().size());
 	}
 	
-	@Test
-	public void testAttributesReleasetranslationRelease() throws SolrServerException, IOException {
-		FilexmlSourceClasspath repoSource = new FilexmlSourceClasspath("se/simonsoft/cms/indexing/xml/datasets/releasetranslation");
-		CmsRepositoryFilexml repo = new CmsRepositoryFilexml("http://localtesthost/svn/testaut1", repoSource);
-		FilexmlRepositoryReadonly filexml = new FilexmlRepositoryReadonly(repo);
-		
-		SolrClient reposxml = indexing.enable(new ReposTestBackendFilexml(filexml)).getCore("reposxml");
-		
-		SolrDocument elem;
-		// search for the first title
-		SolrDocumentList findUsingRid = reposxml.query(new SolrQuery("a_cms.rid:2gyvymn15kv0001 AND -prop_abx.TranslationLocale:*")).getResults();
-		assertEquals("Should find the first title in the release (though actually a future one)", 1, findUsingRid.getNumFound());
-		elem = findUsingRid.get(0);
-		assertEquals("get the rid attribute", "2gyvymn15kv0001", elem.getFieldValue("a_cms.rid"));
-		assertEquals("get the parent rlogicalid", "x-svn:///svn/testaut1^/tms/xml/Docs/My%20First%20Novel.xml?p=5", elem.getFieldValue("ia_cms.rlogicalid"));
-
-		
-		findUsingRid = reposxml.query(new SolrQuery("a_cms.rid:2gyvymn15kv0006 AND -prop_abx.TranslationLocale:*")).getResults();
-		assertEquals("Should find a para", 1, findUsingRid.getNumFound());
-		elem = findUsingRid.get(0);
-		assertEquals("verify it is a para", "p", elem.getFieldValue("name")); 
-		assertEquals("get the rid attribute", "2gyvymn15kv0006", elem.getFieldValue("a_cms.rid")); 
-		assertEquals("get the ancestor rid attribute (in this case parent rid)", "2gyvymn15kv0004", elem.getFieldValue("aa_cms.rid"));
-		assertEquals("get the inherited rid attribute (in this case context element rid)", "2gyvymn15kv0006", elem.getFieldValue("ia_cms.rid"));
-		assertEquals("get the root rid attribute", "2gyvymn15kv0000", elem.getFieldValue("ra_cms.rid"));
-		assertEquals("get the preceding sibling rid attribute", "2gyvymn15kv0005", elem.getFieldValue("sa_cms.rid"));
-		assertNull("get the project id attribute", elem.getFieldValue("a_cms.translation-project"));
-		
-		assertEquals("get the inherited rlogicalid attribute", "x-svn:///svn/testaut1^/tms/xml/Secs/First%20chapter.xml?p=4", elem.getFieldValue("ia_cms.rlogicalid"));
-		
-		assertEquals("assist depends on patharea", Arrays.asList(new String[] {"release"}), elem.getFieldValue("patharea"));
-		assertEquals("assist depends on reusevalue even for a Release", 1, elem.getFieldValue("reusevalue"));		
-	}
-	
 	@SuppressWarnings("unchecked")
 	@Test
 	public void testAttributesReleasetranslationTranslation() throws SolrServerException, IOException {
